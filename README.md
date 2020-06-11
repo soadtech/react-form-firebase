33498dc1ab0d7cae6cd9a3efeba3e1fd6b8e0bee
soadtech <fernandojoseropero@gmail.com>
2020-06-10 20:47:33 -0500

    se actualizo con hooks y se modularizo

--- README.md
+++ README.md
@@ -1,28 +1,51 @@
 # react-form-firebase
+
 Simple form with Firebase
 
 ![React Form Firebase](https://s3.amazonaws.com/chewiekie/react-form-firebase.jpg)
 
+### Do not forget
+
+```
+npm install firebase
+```
+
+and
+
+```
+firebase login
+```
+
+```
+firebase init
+```
 
 ### Install Dependencies
+
 ```
 npm install
 ```
 
 ### Starting the Server
-Run the next command to launch the development server. 
+
+Run the next command to launch the development server.
+
 ```
 npm start
 ```
 
 ### Building for Production
+
 To build an optimized bundle, run
+
 ```
 npm run build
 ```
 
 ## Demo
+
 [demo](https://gndx-projects.firebaseapp.com/)
 
 ## Info
+
 [Create a contact Form with React + Firebase](https://gndx.co/formulario-de-contacto-con-react-firebase/)
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,15 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-import firebase from 'firebase';
-
-const config = {
-    apiKey: "unreadablestuff",
-    authDomain: "your-project-name.firebaseapp.com",
-    databaseURL: "https://your-project-name.firebaseio.com",
-    projectId: "your-project-name",
-    storageBucket: "your-project-name.appspot.com",
-    messagingSenderId: "0112358132134"
+const firebaseConfig = {
+  apiKey: "unreadablestuff",
+  authDomain: "your-project-name.firebaseapp.com",
+  databaseURL: "https://your-project-name.firebaseio.com",
+  projectId: "your-project-name",
+  storageBucket: "your-project-name.appspot.com",
+  messagingSenderId: "0112358132134",
+  appId: "1:000000002:web:0000000000000f",
+  measurementId: "G-000000000",
 };
 
-const firebaseConf = firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
-export default firebaseConf;
+export default firebase;
